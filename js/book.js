0604c695bb3fb83d0ea92addfdbbd2697f1d7d16
Sebastiaan Verbeek <contact@svatwork.com>
2020-05-03 11:21:05 +0700

    feat: BookHeader inherited object for table header

--- js/book.js
+++ js/book.js
@@ -9,3 +9,12 @@ function Book(title, author, nPages, isRead) {
     this.nPages = nPages;
     this.isRead = isRead;
 }
+
+function BookHeader() {
+    this.title = "Title";
+    this.author = "Author";
+    this.nPages = "Number of Pages";
+    this.isRead = "Has been read";
+}
+
+BookHeader.prototype = Object.create(Book.prototype);
