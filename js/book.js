23299e455f8bf809abe56f523faded6d8d38d604
Sebastiaan Verbeek <contact@svatwork.com>
2020-05-03 11:20:09 +0700

    fix: removed unnecessary info method

--- js/book.js
+++ js/book.js
@@ -1,18 +1,11 @@
 let myLibrary = [];
+function addBookToLibrary (book) {
+    myLibrary.push(book);
+}
 
 function Book(title, author, nPages, isRead) {
     this.title = title;
     this.author = author;
     this.nPages = nPages;
     this.isRead = isRead;
-    this.info = function() {
-        let isReadStr = "";
-        if (isRead) isReadStr = "read";
-        else isReadStr = "not read yet";
-        return this.title + " by " + this.author + ", " + this.nPages + " pages, " + isReadStr + ".";
-    }
-}
-
-function addBookToLibrary(book) {
-    myLibrary.push(book);
 }
