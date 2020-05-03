599afd6e1d40d8524ebd0602933626700fec61f3
Sebastiaan Verbeek <contact@svatwork.com>
2020-05-03 11:47:52 +0700

    feat: display table on the page

--- js/display.js
+++ js/display.js
@@ -1,1 +1,9 @@
 addBookToLibrary(new Book("The Hobbit", "Tolkien", 300, true));
+
+function render(library, table) {
+    library.forEach(function (book) {
+        table.appendChild(book.renderTableRow());
+    });
+}
+
+render(myLibrary, document.getElementsByTagName("table")[0])
