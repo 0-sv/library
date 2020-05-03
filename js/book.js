54ea3b19d1f3a22a96f48af1dd82cfdc3b5b2b12
Sebastiaan Verbeek <contact@svatwork.com>
2020-05-03 10:35:26 +0700

    feat: skeleton html and js

---
+++ js/book.js
@@ -0,0 +1,18 @@
+let myLibrary = [];
+
+function Book(title, author, nPages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.nPages = nPages;
+    this.isRead = isRead;
+    this.info = function() {
+        let isReadStr = "";
+        if (isRead) isReadStr = "read";
+        else isReadStr = "not read yet";
+        return this.title + " by " + this.author + ", " + this.nPages + " pages, " + isReadStr + ".";
+    }
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
